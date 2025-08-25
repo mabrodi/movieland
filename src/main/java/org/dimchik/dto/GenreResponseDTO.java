5bfbd4f7417697455cc7ac50dd87373a2684d04c
dimaperehuda <mabrodi7@gmail.com>
2025-08-25 21:28:20 +0300

    add dto

---
+++ src/main/java/org/dimchik/dto/GenreResponseDTO.java
@@ -0,0 +1,14 @@
+package org.dimchik.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenreResponseDTO {
+    private long id;
+    private String name;
+}
---
+++ src/main/java/org/dimchik/dto/MovieResponseDTO.java
@@ -0,0 +1,18 @@
+package org.dimchik.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieResponseDTO {
+    private long id;
+    private String nameRussian;
+    private String nameNative;
+    private int yearOfRelease;
+    private double rating;
+    private double price;
+    private String picturePath;
+}
