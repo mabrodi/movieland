3702ee1a2fc84fa179a644912c7ccc94411dd1e4
dimaperehuda <mabrodi7@gmail.com>
2025-08-25 21:26:24 +0300

    add entity user

---
+++ src/main/java/org/dimchik/entity/User.java
@@ -0,0 +1,23 @@
+package org.dimchik.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
