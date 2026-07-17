e7138495d8a084656b0dff5b2618c4d74b378ee9
mabrodi <mabrodi7@gmail.com>
2026-07-17 12:32:44 +0300

    rounding price for currency

--- src/main/java/org/dimchik/service/impl/CurrencyServiceImpl.java
+++ src/main/java/org/dimchik/service/impl/CurrencyServiceImpl.java
@@ -1,14 +1,17 @@
 package org.dimchik.service.impl;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.dimchik.dto.response.MovieDetailResponse;
 import org.dimchik.enums.Currency;
 import org.dimchik.service.CurrencyService;
 import org.dimchik.service.cache.RateCacheService;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CurrencyServiceImpl implements CurrencyService {
     private final RateCacheService rateCacheService;
 
@@ -17,6 +20,14 @@ public class CurrencyServiceImpl implements CurrencyService {
         rateCacheService.findAll().stream()
                 .filter(rate -> currency.name().equals(rate.getCurrency()))
                 .findFirst()
-                .ifPresent(rate -> movieDetailResponse.setPrice(movieDetailResponse.getPrice() * rate.getRate()));
+                .ifPresent(rate -> {
+                    BigDecimal rateMultiplier = BigDecimal.valueOf(rate.getRate());
+                    BigDecimal price = BigDecimal.valueOf(movieDetailResponse.getPrice());
+                    BigDecimal finalPrice = price
+                            .multiply(rateMultiplier)
+                            .setScale(2, RoundingMode.HALF_UP);
+
+                    movieDetailResponse.setPrice(finalPrice.doubleValue());
+                });
     }
 }
